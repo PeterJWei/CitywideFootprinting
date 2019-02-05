b7d4cd2c95fad4919861ffb0af1e2ab82a8ace5f
peterwei <pw2428@columbia.edu>
2019-02-05 16:03:53 -0500

    reduced boston data by 10

--- static/BostonData/parseCSV.py
+++ static/BostonData/parseCSV.py
@@ -24,17 +24,44 @@ class loadCuebiq:
 			csvwriter.writerow(["latitude", "longitude", "hour", "minute"])
 			for i in range(len(self.times)):
 				csvwriter.writerow(list(self.times[i]))
-		
-L = loadCuebiq()
 
-directory = "/Users/peterwei/Desktop/CitywideFootprinting/static/data/"
-fileNo=0
-for filename in os.listdir(directory):
-    if filename.endswith(".csv"):
-    	print("File Number: " + str(fileNo))
-    	L.loadRoutes(directory+filename)
-        # print(os.path.join(directory, filename))
-        fileNo += 1
-L.saveRoutes("bostonCuebiq.csv")
-#L.loadRoutes("part-00000")
-#L.saveRoutes("testTimes.csv")
+def launchCuebiq():
+	L = loadCuebiq()
+
+	directory = "/Users/peterwei/Desktop/CitywideFootprinting/static/data/"
+	fileNo=0
+	for filename in os.listdir(directory):
+	    if filename.endswith(".csv") and fileNo % 10 == 0:
+	    	print("File Number: " + str(fileNo))
+	    	L.loadRoutes(directory+filename)
+	        # print(os.path.join(directory, filename))
+	    fileNo += 1
+	L.saveRoutes("bostonCuebiq.csv")
+
+launchCuebiq()
+
+class checkCuebiq:
+	def check(self, fileName):
+		csvFile = fileName# + ".csv"
+		with open(csvFile, 'rb') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			first = True
+			for line in reader:
+				if first:
+					first = False
+					continue
+				lat = float(line[0])
+				lon = float(line[1])
+				if lat > 43.0 or lat < 42.0 or lon > -70 or lon < -72:
+					print((lat, lon))
+
+				hour = int(line[2])
+				if hour > 23 or hour < 0:
+					print("Hour: " + str(hour))
+				minute = int(line[3])
+				if minute > 59 or minute < 0:
+					print("Minute: " + str(minute))
+
+C = checkCuebiq()
+C.check("bostonCuebiq.csv")
+
