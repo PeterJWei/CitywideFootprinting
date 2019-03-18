713d47f98e025510333f51efbe7b09cd2ef2e8a4
peterwei <pw2428@columbia.edu>
2019-03-17 23:11:59 -0400

    added database files

---
+++ DBMgr.py
@@ -0,0 +1,19 @@
+import pymongo
+import datetime
+import time
+
+
+
+class DBMgr(object):
+	def __init__(self, start_bg_thread=True):
+		self.dbc=pymongo.MongoClient()
+		db1 = self.dbc.test_database
+		self.coords = db1.coordCollection
+
+	def recordCoordinates(self, user, lat, lon):
+		self.coords.insert({
+			"timestamp": datetime.datetime.now()
+			"user": user,
+			"lat": lat,
+			"lon": lon
+			})
